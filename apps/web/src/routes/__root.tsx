68c290749520b73aef540e3255df1770bcec948d
Sandip Sapkota <sandipsapkota9844@gmail.com>
2025-11-26 15:10:50 +0545

    feat: Configure ClerkProvider with `VITE_CLERK_PUBLISHABLE_KEY` and add validation for its presence.

--- apps/web/src/routes/__root.tsx
+++ apps/web/src/routes/__root.tsx
@@ -64,8 +64,13 @@ export const Route = createRootRouteWithContext<RouterAppContext>()({
 });
 function RootDocument() {
 	const context = useRouteContext({ from: Route.id });
+	const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+	if (!PUBLISHABLE_KEY) {
+		throw new Error('Add your Clerk Publishable Key to the .env file')
+	}
 	return (
-		<ClerkProvider >
+		<ClerkProvider publishableKey={PUBLISHABLE_KEY}>
 			<ConvexProviderWithClerk client={context.convexClient} useAuth={useAuth}>
 				<html lang="en" className="dark">
 					<head>
